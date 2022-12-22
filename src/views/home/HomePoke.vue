9927c44f82cac2acc686797a4c2e589a6c3a8532
Lucas <lucas.tallos@tallos.com.br>
2022-12-22 10:38:33 -0300

    feat: adding statistics about life and score

--- src/views/home/HomePoke.vue
+++ src/views/home/HomePoke.vue
@@ -4,6 +4,10 @@
       <img src="@/assets/img/pokedex.png" class="pokeimg" />
       <div class="FormD">
         <b-form><input type="text" name="" id="" class="inputP shadow-none"/></b-form>
+        <div class="d-flex justify-content-between pt-4">
+          <div class="status d-flex justify-content-center align-items-center"><h6>Life: </h6></div>
+          <div class="status d-flex justify-content-center align-items-center"><h6>Score: </h6></div>
+        </div>
       </div>
     </div>
   </div>
